7f68a78bdc233234feaa346345286d3199d6f308
karimelhage <karimelhage94@gmail.com>
2023-04-17 20:07:51 +0200

    add txt to return description

--- plot_utils.py
+++ plot_utils.py
@@ -7,7 +7,7 @@ def plot_losses(total_train_losses, total_val_losses):
 
     :param total_train_losses:  list of training losses found in training
     :param total_val_losses: list of validation losses found in training
-    :return:
+    :return: plot of losses during training
     '''
     plt.figure(figsize=(10, 8))
     plt.plot(list(range(len(total_train_losses) + 1))[1:120], total_train_losses[:120])
