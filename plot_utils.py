bc639d5c3859ee5f3da3e89784208a8260309d0c
karimelhage <karimelhage94@gmail.com>
2023-04-17 15:41:31 +0200

    edit function plot_losses desc

--- plot_utils.py
+++ plot_utils.py
@@ -2,6 +2,13 @@ import matplotlib.pyplot as plt
 
 
 def plot_losses(total_train_losses, total_val_losses):
+    '''
+    Function to plot the losses found during model training
+
+    :param total_train_losses:  list of training losses found in training
+    :param total_val_losses: list of validation losses found in training
+    :return:
+    '''
     plt.figure(figsize=(10, 8))
     plt.plot(list(range(len(total_train_losses) + 1))[1:120], total_train_losses[:120])
     plt.plot(list(range(len(total_train_losses) + 1))[1:120], total_val_losses[:120])
