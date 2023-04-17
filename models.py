991bbe94fd9f30ad4a513460d250386a3490233b
karimelhage <karimelhage94@gmail.com>
2023-04-17 15:57:36 +0200

    change dice score function

--- models.py
+++ models.py
@@ -3,12 +3,13 @@ from tqdm import tqdm
 import torch
 from torch.nn.functional import one_hot  # function to one-hot encode labels
 import torch.nn as nn
-from metrics import f1_dice_score, accuracy
+from metrics import weighted_f1, accuracy
 
 
 def train_UNet(model, optimizer, epochs, train_dataloader, val_dataloader, scheduler, device='cpu'):
     '''
-    Function to try smp U-Net model
+    Function to train smp U-Net model
+
     :param model: The input U-Net model to train
     :param optimizer: torch.optim - the optimizer to be used to train
     :param epochs: int - the number of training epochs
@@ -34,25 +35,25 @@ def train_UNet(model, optimizer, epochs, train_dataloader, val_dataloader, sched
 
         for i, batch, in enumerate(tqdm(train_dataloader)):
             img_batch, lbl_batch = batch
-            img_batch = torch.flatten(img_batch, 0, 1)
-            lbl_batch = torch.flatten(lbl_batch, 0, 1)
-            lbl_batch_1hot = one_hot(lbl_batch, 27)
-            lbl_batch_1hot = lbl_batch_1hot.permute(0, 3, 1, 2)
+            img_batch = torch.flatten(img_batch, 0,
+                                      1)  # since each entru in batch has 6 patches, tensor flattened to instead have 6 patch * batch_size
+            lbl_batch = torch.flatten(lbl_batch, 0, 1)  # same as above
+            lbl_batch_1hot = one_hot(lbl_batch,
+                                     27)  # masks one-hot encoded (only to be used in calculating cross entropy loss between prediction of classes and true class)
+            lbl_batch_1hot = lbl_batch_1hot.permute(0, 3, 1,
+                                                    2)  # one-hot encoded mask reshaped to (num_masks_in_batch,27,image_width)
             lbl_batch_1hot = lbl_batch_1hot.float()
-            # img_batch, lbl_batch_1hot = img_batch.to(device), lbl_batch.to(device)
             img_batch, lbl_batch_1hot = img_batch.to(device), lbl_batch_1hot.to(device)
-            ##implement the forward and backward backpropagation
-            # *****START CODE
+
             optimizer.zero_grad()
             outputs = model(img_batch)
 
             loss = criterion(outputs, lbl_batch_1hot)
             loss.backward()
             optimizer.step()
-            # *****END CODE
 
-            f1 = f1_dice_score(outputs, lbl_batch.to(device))
-            acc = accuracy(outputs, lbl_batch.to(device))
+            f1 = weighted_f1(outputs, lbl_batch.to(device), device)
+            acc = accuracy(outputs, lbl_batch.to(device), device)
             train_losses.append(loss.item())
             train_accuracy.append(acc)
             train_f1.append(f1)
@@ -77,18 +78,29 @@ def train_UNet(model, optimizer, epochs, train_dataloader, val_dataloader, sched
             lbl_batch_1hot = lbl_batch_1hot.permute(0, 3, 1, 2)
             lbl_batch_1hot = lbl_batch_1hot.float()
             img_batch, lbl_batch_1hot = img_batch.to(device), lbl_batch_1hot.to(device)
-            ##pass the images to the model and calculate the loss
-            # *****START CODE
+
             with torch.no_grad():
                 outputs = model(img_batch)
                 loss = criterion(outputs, lbl_batch_1hot)
-            # *****END CODE
 
-            f1 = f1_dice_score(outputs, lbl_batch.to(device))
-            acc = accuracy(outputs, lbl_batch.to(device))
+            f1 = weighted_f1(outputs, lbl_batch.to(device), device)
+            acc = accuracy(outputs, lbl_batch.to(device), device)
             val_losses.append(loss.item())
             val_accuracy.append(acc)
             val_f1.append(f1)
+
+        print(
+            f'VAL       Epoch: {epoch} | Epoch metrics | loss: {np.mean(val_losses):.4f}, f1: {np.mean(val_f1):.3f}, accuracy: {np.mean(val_accuracy):.3f}')
+        total_val_losses.append(np.mean(val_losses))
+        total_val_accuracy.append(np.mean(val_accuracy))
+        total_val_f1.append(np.mean(val_f1))
+        scheduler.step()
+
+        # Saving the best models if meeting min f1 criteria (this is updated per model trained and epoch)
+        if np.mean(val_f1) > min_val_f1:
+            torch.save(model.state_dict(),
+                       f'/content/drive/MyDrive/FDL/Hurricane Harvey/Models/Unet_pat_aug' + str(epoch) + '.pt')
+            min_val_f1 = np.mean(val_f1)
 
         print(
             f'VAL       Epoch: {epoch} | Epoch metrics | loss: {np.mean(val_losses):.4f}, f1: {np.mean(val_f1):.3f}, accuracy: {np.mean(val_accuracy):.3f}')
